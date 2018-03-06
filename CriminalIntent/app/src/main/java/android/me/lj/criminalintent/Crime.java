d877c19e5e4ce9cbbbd72a979b8392f0c45381bb
PPJLiujie <120528456@qq.com>
2018-03-06 15:50:21 +0800

    第八章完毕

--- CriminalIntent/app/src/main/java/android/me/lj/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/android/me/lj/criminalintent/Crime.java
@@ -14,6 +14,11 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
 
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
     public UUID getId() {
         return mId;
     }
