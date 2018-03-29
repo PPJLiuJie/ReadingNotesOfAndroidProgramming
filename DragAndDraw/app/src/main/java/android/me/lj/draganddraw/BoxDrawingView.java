79e858035d5f5a6b00bdbcaa8d8b37e370fdd969
PPJLiujie <120528456@qq.com>
2018-03-29 14:50:30 +0800

    第三十一章完成

---
+++ DragAndDraw/app/src/main/java/android/me/lj/draganddraw/BoxDrawingView.java
@@ -0,0 +1,130 @@
+package android.me.lj.draganddraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/3/29.
+ */
+
+public class BoxDrawingView extends View {
+
+    private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
+
+    /**
+     * 这里之所以添加了两个构造方法，是因为视图可从代码或者布局文件实例化。
+     * 从布局文件中实例化的视图会收到一个AttributeSet实例，该实例包含了XML布局文件中指定的XML属性。
+     * 即使不打算使用构造方法，按习惯做法也应添加这两个构造方法。
+     */
+
+    /**
+     * 使用代码创建View时使用该构造方法
+     */
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    /**
+     * 使用inflate从XML中创建View时使用该构造方法
+     */
+    public BoxDrawingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        /**
+         * 填充背景色
+         */
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            /**
+             * 绘制矩形框
+             */
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+    }
+
+    /**
+     * getRawX()和getRawY()获取的是相对屏幕左上角的坐标
+     * getX()和getY()获取的是相对于当前View左上角的坐标
+     *
+     * Point和PointF的区别：Point使用int类型，PointF使用float类型
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        PointF current = new PointF(event.getX(), event.getY());
+
+        String action = "";
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                /**
+                 * 任何时候，只要接收到ACTION_DOWN动作事件，
+                 * 就以事件原始坐标新建Box对象并赋值给mCurrentBox，然后再添加到矩形框数组中。
+                 */
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                /**
+                 * 用户手指在屏幕上移动时， mCurrentBox.mCurrent会得到更新。
+                 */
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    /**
+                     * invalidate()方法会强制BoxDrawingView重新绘制自己。
+                     * 并再次调用onDraw(Canvas)方法
+                     */
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                /**
+                 * 在取消触摸事件或用户手指离开屏幕时，清空mCurrentBox以结束屏幕绘制。
+                 */
+                mCurrentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                mCurrentBox = null;
+                break;
+        }
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+        return true;
+    }
+}
---
+++ DragAndDraw/app/src/main/java/android/me/lj/draganddraw/SingleFragmentActivity.java
@@ -0,0 +1,68 @@
+package android.me.lj.draganddraw;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/3/5.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        /**
+         因为使用了支持库及AppCompatActivity类， 所以这里调用了getSupportFragmentManager()
+         方法。如果不考虑旧版本的兼容性问题，可直接继承Activity类并调用getFragmentManager()
+         方法。
+         */
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        /**
+         这里为什么先获取fragment，然后对其判空？
+         设备旋转或回收内存时， Android系统会销毁CrimeActivity，
+         而后重建时，会调用CrimeActivity.onCreate(Bundle)方法。 activity被
+         销毁时，它的FragmentManager会将fragment队列保存下来。这样， activity重建时，新的
+         FragmentManager会首先获取保存的队列，然后重建fragment队列，从而恢复到原来的状态。
+         */
+        if (fragment == null) {
+            fragment = createFragment();
+
+            /**
+             fragment事务被用来添加、移除、附加、分离或替换fragment队列中的fragment。这是使用
+             fragment动态组装和重新组装用户界面的关键。 FragmentManager管理着fragment事务回退栈。
+
+             add(...)方法是整个事务的核心，它有两个参数：容器视图资源ID和新创建的CrimeFragment。
+
+             容器视图资源ID的作用有：
+             1. 告诉FragmentManager， fragment视图应该出现在activity视图的什么位置；
+             2. 唯一标识FragmentManager队列中的fragment。
+             如需从FragmentManager中获取CrimeFragment，使用容器视图资源ID就行了：
+             FragmentManager fm = getSupportFragmentManager();
+             Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+             */
+
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    /**
+     * 使用了@LayoutRes注解。这告诉Android Studio，任何时候，该实现方法都应该返回有效的布局资源ID。
+     */
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    protected abstract Fragment createFragment();
+}
